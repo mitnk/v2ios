b52cad4ef4b56ea843ba940ca4e11d1e86e1e900
mitnk <whgking@gmail.com>
2012-07-17 16:02:30 +0800

    code generated by Xcode 4.3.1

---
+++ HelloWorld/HelloWorldAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldAppDelegate.h
+//  HelloWorld
+//
+//  Created by mitnk on 7/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by mitnk on 7/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
