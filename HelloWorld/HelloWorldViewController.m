dce40b47887d12cf0bdcb41910d954eeb27e82a3
mitnk <whgking@gmail.com>
2012-07-18 14:36:15 +0800

    finished this demo.

--- HelloWorld/HelloWorldViewController.m
+++ HelloWorld/HelloWorldViewController.m
@@ -15,11 +15,12 @@
 @implementation HelloWorldViewController
 @synthesize label;
 @synthesize textField;
+@synthesize userName = _userName;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)viewDidUnload
@@ -36,5 +37,19 @@
 }
 
 - (IBAction)changeGreeting:(id)sender {
+    self.userName = self.textField.text;
+    NSString *nameString = self.userName;
+    if ([nameString length] == 0) {
+        nameString = @"World";
+    }
+    NSString *greeting = [[NSString alloc] initWithFormat:@"Hello, %@!", nameString];
+    self.label.text = greeting;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)theTextField {
+    if (theTextField == self.textField) {
+        [theTextField resignFirstResponder];
+    }
+    return YES;
 }
 @end
