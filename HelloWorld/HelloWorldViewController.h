8f5d84fa0c8b5806a833c7b06e26f6829434b8f0
mitnk <whgking@gmail.com>
2012-07-18 16:01:44 +0800

    added ASIHTTPRequest, SBJson, and stringByAddingPercentEscapesUsingEncoding demos.

--- HelloWorld/HelloWorldViewController.h
+++ HelloWorld/HelloWorldViewController.h
@@ -10,8 +10,8 @@
 
 @interface HelloWorldViewController : UIViewController <UITextFieldDelegate>
 
-@property (weak, nonatomic) IBOutlet UILabel *label;
-@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (assign, nonatomic) IBOutlet UILabel *label;
+@property (assign, nonatomic) IBOutlet UITextField *textField;
 @property (copy, nonatomic) NSString *userName;
 - (IBAction)changeGreeting:(id)sender;
 @end
--- HelloWorld/HelloWorldViewController.m
+++ HelloWorld/HelloWorldViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "HelloWorldViewController.h"
+#import "ASIHTTPRequest.h"
+#import "SBJSON.h"
 
 @interface HelloWorldViewController ()
 
@@ -42,8 +44,23 @@
     if ([nameString length] == 0) {
         nameString = @"World";
     }
-    NSString *greeting = [[NSString alloc] initWithFormat:@"Hello, %@!", nameString];
-    self.label.text = greeting;
+    nameString = [nameString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    self.label.text = nameString;
+    
+    NSString *api_url = [[NSString alloc] initWithFormat:@"http://translate.google.com/translate_a/t?text=%@&client=it&hl=en&ie=UTF-8&oe=UTF-8&sl=en&tl=zh-CN", nameString];
+
+    NSURL *url = [NSURL URLWithString:api_url];
+    ASIHTTPRequest *request = [ASIHTTPRequest requestWithURL:url];
+    [request startSynchronous];
+    NSError *error = [request error];
+    if (!error) {
+        NSString *response = [request responseString];
+        NSDictionary *responseDict = [response JSONValue];
+        self.label.text = [[[responseDict valueForKey:@"sentences"] objectAtIndex:0] valueForKey:@"translit"];
+    }
+    else {
+        self.label.text = @"Error!";
+    }
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)theTextField {
